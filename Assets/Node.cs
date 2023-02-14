872b9d1333da875a694c7b7181a39af0e9046327
Erudox <66876173+JaimeDV@users.noreply.github.com>
2023-02-15 00:54:20 +0100

    A estrella funcional
    Save encontrar caminos y los muestra

---
+++ Assets/Node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public int gridX;
+    public int gridY;
+
+    public bool isWall;
+    public Vector3 position;
+
+    public Node parentNode;
+
+    public int igCost;
+    public int ihCost;
+
+    public int FCost { get { return igCost + ihCost; } }
+
+    public Node(bool isWall, Vector3 a_vPos, int gridX, int gridY)
+    {
+        this.isWall = isWall;
+        this.position = a_vPos;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+}
